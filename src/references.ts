69e1a94da884aa62548c7b81dbb3f62a36fd3f8f
Teju Nareddy <tejunareddy@gmail.com>
2021-03-07 20:24:10 -0500

    Add references for defects user warranties

--- src/references.ts
+++ src/references.ts
@@ -3,10 +3,11 @@ import { Reference } from "./types"
 // Records should be added here to be indexed / made searchable
 const references: Array<Reference> = [
   {
-    collection: "example",
-    index: "example",
-    include: ["name", "email", "biography", "updatedAt", "createdAt"],
-  },
+    collection: "users",
+    subcollection: "warranties",
+    index: "user-warranties",
+    builder: (ref) => ref.where('paid', '==', true),
+  }
 ]
 
 export default references
